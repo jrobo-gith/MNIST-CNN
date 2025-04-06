e320c96a2a68adc872a0cf23489378ed212b53d1
jrobo-gith <jacktheroberts10@gmail.com>
2025-04-06 23:50:18 +0100

    Removed commented stuff out

--- CNN_training.py
+++ CNN_training.py
@@ -48,29 +48,3 @@ for epoch in range(num_epochs):
         print(f"Epoch {epoch+1}/{num_epochs} | Batch {i+1}/{len(train_loader)} | Loss: {loss.item():.4f}", end="\r")
 
 torch.save(model.state_dict(), 'model/cnn_weights.pth')
-
-
-# # Optional: Save class definition separately or import in evaluation script
-# torch.save(CNN, 'model.pt')
-
-# test_dataset = torchvision.datasets.MNIST(root='../../data/',
-#                                           train=False,
-#                                           transform=transforms.ToTensor())
-#
-# test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-#                                           batch_size=batch_size,
-#                                           shuffle=False)
-#
-# model.eval()
-# with torch.no_grad():
-#     correct = 0
-#     total = 0
-#     for images, labels in test_loader:
-#         images = images.to(device)
-#         labels = labels.to(device)
-#         outputs = model(images)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += (predicted == labels).sum().item()
-#
-#     print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
